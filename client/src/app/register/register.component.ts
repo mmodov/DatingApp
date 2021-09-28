6400ca2a7b7dbe3ca14c077f16627c1329c1a861
Madalina Moldoveanu <madalina_moldoveanu@trimble.com>
2021-09-28 09:49:37 +0300

    add dating app

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+  registerForm: FormGroup;
+
+  constructor(private accountService: AccountService, private toastr: ToastrService ) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup( {
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    })
+
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+      ? null : {isMatching: true}
+    }
+  }
+
+  register() {
+    console.log(this.registerForm.value);
+   /* this.accountService.register(this.model).subscribe(respose => {
+      console.log(respose);
+      this.cancel();
+    }, error => {
+      console.log(error)
+      this.toastr.error(error.error);
+    });*/
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
